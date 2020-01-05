deefd5496b39d4711d92617f25b9f55d3f95f914
A <adarshcool97@gmail.com>
2020-01-05 15:45:36 +0530

    reversed the timezone on modify-pricing for uniformity.

--- admin/modify-pricing.php
+++ admin/modify-pricing.php
@@ -7,8 +7,8 @@ include('includes/config.php');
 // header('location:index.php');
 // }
 // else{
-date_default_timezone_set('America/Los_Angeles');// change according timezone
-$currentTime = date( 'm-d-Y h:i:s A', time () );
+date_default_timezone_set('Asia/Kolkata');// change according timezone
+$currentTime = date( 'd-m-Y h:i:s A', time () );
 $id=intval($_GET['id']);
 
 if(isset($_POST['submit']))
