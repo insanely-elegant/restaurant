436f0ee41fd39c93f85df81f07e7eb7889f18fe5
Avinash M G <hatman290@gmail.com>
2020-01-06 20:44:26 +0530

    Disable insert of the table layout if already present in the database

--- admin/get_totaltable.php
+++ admin/get_totaltable.php
@@ -9,9 +9,16 @@ $query=mysqli_query($con,"select * from room where id='$id'");
 <?php
  while($row=mysqli_fetch_array($query))
  {
-  ?>
-  <option selected="true" disabled="disabled" value="<?php echo htmlentities($row['totaltables']); ?>"><?php echo htmlentities($row['totaltables']); ?></option>
-  <?php
+   $result = $con->query("select count(*) from tablelayout where roomid='$id'")->fetch_assoc()['count(*)'];
+   if($result==0){
+     ?>
+     <option selected="true" disabled="disabled" value="<?php echo htmlentities($row['totaltables']); ?>"><?php echo htmlentities($row['totaltables']); ?></option>
+     <?php
+   }else{
+     ?>
+     <script>alert("A Layout Exists For This Room")</script>
+     <?php
+   }
 
 }
 }
