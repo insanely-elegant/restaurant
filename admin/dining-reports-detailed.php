60f0c5bbeb028c2d2f5c4bcc49ddf5f72cb63f03
A <adarshcool97@gmail.com>
2020-01-07 05:57:45 +0530

    stylized export button

--- admin/dining-reports-detailed.php
+++ admin/dining-reports-detailed.php
@@ -91,24 +91,34 @@ while($row=mysqli_fetch_array($query))
                                         <div class="panel-body">
 <div class="row">
 <div class="col-md-12">
-    
-<input type="button" onclick="tableToExcel('sample-table-1', 'W3C Example Table')" value="Export to Excel">
+</br>
+<input class="btn btn-o black" type="button" onclick="tableToExcel('sample-table-1', 'W3C Example Table')" value="Export to Excel"></br> </br>
 <?php
 $fdate=$_POST['fromdate'];
 $tdate=$_POST['todate'];
 
 ?>
-<h5 align="center" style="color:blue">Dining Report from <?php echo $fdate?> to <?php echo $tdate?></h5>
-	
-<table class="table table-hover" id="sample-table-1">
+<h5 align="center" style="color:blue">Dining Report from <?php echo $fdate?> to <?php echo $tdate?></h5> </br>
+<div class="card-body">
+<div class="form-group">
+    <div align="left"> Search by either filters below: </div>  </br>
+    <input  class="form-control" type="text" id="myInput" onkeyup="myFunction()" placeholder="First name" title="Type in a first name"> </div>
+  
+   <div class="form-group">
+    <input  class="form-control" type="text" id="myInput2" onkeyup="myFunction2()" placeholder="Last name" title="Type in a last name"></div>
+       
+    <div class="form-group">
+    <input  class="form-control" type="text" id="myInput3" onkeyup="myFunction3()" placeholder="Dish name" title="Type in the dish name"></div>
+</div>
+    <table class="table table-hover" id="sample-table-1">
 <thead>
 <tr>
 <th class="center">#</th>
 <th>First Name</th>
 <th>Last Name</th>
-<th>Room</th>
 <th>Table Name </th>
 <th>Dining Date </th>
+<th>Dish Name</th>
 </tr>
 </thead>
 <tbody>
@@ -123,9 +133,9 @@ while($row=mysqli_fetch_array($sql))
 <td class="center"><?php echo $cnt;?>.</td>
 <td class="hidden-xs"><?php echo $row['firstname'];?></td>
 <td><?php echo $row['lastname'];?></td>
-<td><?php echo $row['room'];?></td>
 <td><?php echo $row['tablename'];?></td>
 <td><?php echo $row['diningdate'];?>
+<td><?php echo $row['dishname'];?>
 </td>
 <td>
 
@@ -137,6 +147,66 @@ while($row=mysqli_fetch_array($sql))
 $cnt=$cnt+1;
  }?></tbody>
 </table>
+<script>
+function myFunction() {
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("sample-table-1");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[1];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }       
+  }
+}
+
+function myFunction2() {
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("myInput2");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("sample-table-1");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[2];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }       
+  }
+}
+
+function myFunction3() {
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("myInput3");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("sample-table-1");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[5];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }       
+  }
+}
+
+</script>
+
 </div>
 </div>
 </div>
