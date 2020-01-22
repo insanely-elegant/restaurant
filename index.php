50df4f31432702ab005cea7256545007a5d63198
Avinash <hatman290@gmail.com>
2020-01-22 18:20:55 +0530

    Email added partially

--- index.php
+++ index.php
@@ -17,6 +17,7 @@ if(isset($_POST['login']))
 $_SESSION['id']=$num['id'];
  $_SESSION['firstname']=$num['firstname'];
  $_SESSION['lastname']=$num['lastname'];
+ $_SESSION['email']=$num['email'];
 $uip=$_SERVER['REMOTE_ADDR'];
 $status=1;
  $log=mysqli_query($con,"insert into userlog(unitno,userip,status) values('".$_SESSION['login']."','$uip','$status')");
@@ -44,7 +45,7 @@ exit();
 	<title>Silver Glen - Login</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-<!--===============================================================================================-->	
+<!--===============================================================================================-->
 	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
 <!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
@@ -54,13 +55,13 @@ exit();
 	<link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
 <!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
-<!--===============================================================================================-->	
+<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
 <!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
 <!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
-<!--===============================================================================================-->	
+<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
 <!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="css/util.css">
@@ -68,13 +69,13 @@ exit();
 <!--===============================================================================================-->
 </head>
 <body>
-	
+
 	<div class="limiter">
 		<div class="container-login100">
 			<div class="wrap-login100 p-t-50 p-b-90">
-						
+
 				<form method="post" class="login100-form validate-form flex-sb flex-w">
-				
+
 					<span class="login100-form-title p-b-51">
 						Login
 					</span>
@@ -82,18 +83,18 @@ exit();
 							<?php echo htmlentities($_SESSION['errmsg']); 	?>
 							<?php echo htmlentities($_SESSION['errmsg']=""); ?>
 						</span>
-					
+
 					<div class="wrap-input100 validate-input m-b-16" data-validate = "Unit Number is required">
 						<input class="input100" autofocus type="text" name="unitno" placeholder="Unit Number">
 						<span class="focus-input100"></span>
 					</div>
-					
-					
+
+
 					<div class="wrap-input100 validate-input m-b-16" data-validate = "Password is required">
 						<input class="input100" type="password" name="password" placeholder="Password">
 						<span class="focus-input100"></span>
 					</div>
-					
+
 					<div class="flex-sb-m w-full p-t-3 p-b-24">
 						<div class="contact100-form-checkbox">
 							<input class="input-checkbox100" id="ckb1" type="checkbox" name="remember-me">
@@ -119,10 +120,10 @@ exit();
 			</div>
 		</div>
 	</div>
-	
+
 
 	<div id="dropDownSelect1"></div>
-	
+
 <!--===============================================================================================-->
 	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
 <!--===============================================================================================-->
