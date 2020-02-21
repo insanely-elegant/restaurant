e2053e438a79ad4d144ab046cc2784a1cf95ea75
Adarsh <adarshcool97@gmail.com>
2020-02-22 03:27:29 +0530

    guestlist spacing issue

--- admin/guestlist.php
+++ admin/guestlist.php
@@ -110,8 +110,7 @@ if (isset($_GET['del'])) {
                                         </div>
                                     <?php } ?>
 
-
-
+                                    
 
                                     <div class="module-body table">
                                         <table cellpadding="0" cellspacing="0" border="0" class="datatable-1 table table-bordered table-striped	 display" width="100%">
