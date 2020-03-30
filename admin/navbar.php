6a4dcf27bbf7f49eb4b3ea5f1c31c8e7e7dab7cb
Adarsh <adarshcool97@gmail.com>
2020-03-30 16:50:20 +0530

    Revert "Navbar Fixed"
    This reverts commit 85ab66825a598bbaf1ad2f5b66ae1e3bdd463395.

--- admin/navbar.php
+++ admin/navbar.php
@@ -28,16 +28,16 @@
 <!-- ============================================================== -->
 <!-- left sidebar -->
 <!-- ============================================================== -->
-<div class="nav-left-sidebar sidebar-dark" >
+<div class="nav-left-sidebar sidebar-dark">
     <div class="menu-list">
-        <nav class="navbar navbar-expand-lg navbar-light" style="overflow:scroll">
+        <nav class="navbar navbar-expand-lg navbar-light">
             <a class="d-xl-none d-lg-none" href="dashboard.php">Dashboard</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav flex-column">
-
+                   
                     <li class="nav-item ">
                         <a class="nav-link" href="dashboard.php" aria-expanded="false" data-target="#submenu-1" aria-controls="submenu-1"><i class="fa fa-fw fa-user-circle"></i>Dashboard <span class="badge badge-success">6</span></a>
                     </li>
@@ -177,16 +177,26 @@
                             </ul>
                         </div>
                     </li>
+<<<<<<< HEAD
                  
+=======
+>>>>>>> parent of 85ab668... Navbar Fixed
                     <li class="nav-item">
                         <a class="nav-link" href="#" data-toggle="collapse" aria-expanded="false" data-target="#submenu-13" aria-controls="submenu-13"><i class="fas fa-fw fa-file"></i> Dining Reports </a>
                         <div id="submenu-13" class="collapse submenu" style="">
                             <ul class="nav flex-column">
                                 <li class="nav-item">
+<<<<<<< HEAD
                                     <a class="nav-link" href="dining-reports.php">.</a>
                                 </li>
                                 <li class="nav-item">
                                     <a class="nav-link" href="dining-reports-with-unitno.php">.</a>
+=======
+                                    <a class="nav-link" href="dining-reports.php">View Dining Reports by Date</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="dining-reports-with-unitno.php">View Dining Reports by Date & Unitno</a>
+>>>>>>> parent of 85ab668... Navbar Fixed
                                 </li>
                             </ul>
                         </div>
