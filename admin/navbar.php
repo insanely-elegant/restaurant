85ab66825a598bbaf1ad2f5b66ae1e3bdd463395
Avinash M G <hatman290@gmail.com>
2020-03-30 15:14:39 +0530

    Navbar Fixed

--- admin/navbar.php
+++ admin/navbar.php
@@ -28,16 +28,16 @@
 <!-- ============================================================== -->
 <!-- left sidebar -->
 <!-- ============================================================== -->
-<div class="nav-left-sidebar sidebar-dark">
+<div class="nav-left-sidebar sidebar-dark" >
     <div class="menu-list">
-        <nav class="navbar navbar-expand-lg navbar-light">
+        <nav class="navbar navbar-expand-lg navbar-light" style="overflow:scroll">
             <a class="d-xl-none d-lg-none" href="dashboard.php">Dashboard</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav flex-column">
-                   
+
                     <li class="nav-item ">
                         <a class="nav-link" href="dashboard.php" aria-expanded="false" data-target="#submenu-1" aria-controls="submenu-1"><i class="fa fa-fw fa-user-circle"></i>Dashboard <span class="badge badge-success">6</span></a>
                     </li>
@@ -186,6 +186,19 @@
                                 </li>
                                 <li class="nav-item">
                                     <a class="nav-link" href="dining-reports-with-unitno.php">View Dining Reports by Date & Unitno</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#" data-toggle="collapse" aria-expanded="false" data-target="#submenu-13" aria-controls="submenu-13"><i class="fas fa-fw fa-file"></i> Dining Reports </a>
+                        <div id="submenu-13" class="collapse submenu" style="">
+                            <ul class="nav flex-column">
+                                <li class="nav-item">
+                                    <a class="nav-link" href="dining-reports.php">.</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="dining-reports-with-unitno.php">.</a>
                                 </li>
                             </ul>
                         </div>
