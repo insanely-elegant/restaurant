079d0df9833cc734f4358e7fa14e8915238a55fe
Adarsh <adarshcool97@gmail.com>
2020-05-06 02:01:43 +0530

    Added file directory to show & download backup files

--- admin/show-files.php
+++ admin/show-files.php
@@ -66,7 +66,7 @@ $currentTime = date('d-m-Y h:i:s A', time());
                                         <nav aria-label="breadcrumb">
                                             <ol class="breadcrumb">
                                                 <li class="breadcrumb-item"><a href="dashboard.php" class="breadcrumb-link">Dashboard</a></li>
-                                                <li class="breadcrumb-item active" aria-current="page">Backup & Restore</li>
+                                                <li class="breadcrumb-item active" aria-current="page">Show Files</li>
                                             </ol>
                                         </nav>
                                     </div>
@@ -79,8 +79,8 @@ $currentTime = date('d-m-Y h:i:s A', time());
                             <div class="row">
                                 <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
                                     <div class="section-block" id="basicform">
-                                        <h3 class="section-title">Backup & Restore</h3>
-                                        <p>You can backup & restore data here.</p>
+                                        <h3 class="section-title">Show all files</h3>
+                                        <p>All files in backup directory is shown here. You can click on the file to download</p>
                                     </div>
                                     <?php if (isset($_POST['submit'])) { ?>
                                         <div class="alert alert-success" role="alert">
@@ -98,43 +98,21 @@ $currentTime = date('d-m-Y h:i:s A', time());
                                     <?php } ?>
                                     <div class="card">
                                         <div class="card-body">
-                                            <div class="alert alert-info" role="alert">
-                                                Tip! : Hover on [ ? ] to see info about the feature
-                                            </div>
+
                                             <div class="module-body table">
-                                                <h3 class="section-title">Backup & Restore</h3>
-
-                                                <div class="card-body border-top">
-                                                    <h4>Process & Download Full Database Backup<a data-toggle="tooltip" title="" data-original-title="Click the button below to download a copy of the latest of all the data as a backup file."> [ ? ]</a> <br></h4>
-                                                    <div>
-                                                        <button class="btn btn-outline-primary" onclick="window.location.href = 'download-backup.php';">Process & Download Full Database Backup</button>
-                                                    </div>
-                                                </div>
-                                                <div class="card-body border-top">
-                                                    <h4>Upload Backup to server<a data-toggle="tooltip" title="" data-original-title="Click the button below to upload and store your copy of backup file in server."> [ ? ]</a> <br></h4>
-                                                    <div>
-                                                        <button class="btn btn-outline-dark" onclick="window.location.href = 'continous-backup.php';">Upload Backup to server</button>
-                                                    </div>
-                                                </div>
-                                                <div class="card-body border-top">
-                                                    <h4>Show Backup files<a data-toggle="tooltip" title="" data-original-title="Click here to see all the backup files in server."> [ ? ]</a> <br></h4>
-                                                    <div>
-                                                        <button class="btn btn-outline-success" onclick="window.location.href = 'show-files.php';">Show & Download Backup files</button>
-                                                    </div>
-                                                </div>
-                                                <div class="card-body border-top">
-                                                    <h4>Restore Full Database Backup<a data-toggle="tooltip" title="" data-original-title="Click here to upload & restore to a previous backup"> [ ? ]</a> <br></h4>
-                                                    <div>
-                                                        <button class="btn btn-outline-light" onclick="window.location.href = 'upload-backup.php';">Upload & Restore Full Database Backup</button>
-                                                    </div>
-                                                </div>
-                                                <div class="card-body border-top">
-                                                    <h4>DELETE ALL DATA<a data-toggle="tooltip" title="" data-original-title="Click here to upload & restore to a previous backup"> [ ? ]</a> <br></h4>
-                                                    <div>
-                                                        <button class="btn btn-outline-danger" onclick="window.location.href = 'delete-all-data.php';">DELETE ALL DATA</button>
-                                                    </div>
-                                                </div>
-
+                                                
+                                                <?php
+                                                if ($handle = opendir('backups')) {
+                                                    while (false !== ($file = readdir($handle))) {
+                                                        if ($file != "." && $file != "..") {
+                                                            $thelist .= '<li><a href="backups/' . $file . '">' . $file . '</a></li>';
+                                                        }
+                                                    }
+                                                    closedir($handle);
+                                                }
+                                                ?>
+                                                <h3>Backup files:</h3>
+                                                <ul><?php echo $thelist; ?></ul>
 
 
 
